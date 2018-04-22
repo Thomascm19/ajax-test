a0c6f432691cccf0df0e9ba91b7aae5009b67c15
Thomas <thomascaycedo19@outlook.com>
2018-04-21 19:12:03 -0500

    Agregar archivos de proyecto.

---
+++ ajax-test/Controllers/ConductorsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ajax_test;
+
+namespace ajax_test.Controllers
+{
+    public class ConductorsController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Conductors
+        public ActionResult Index()
+        {
+            return View(db.Conductor.ToList());
+        }
+        // Ajax
+        public ActionResult Listaconductores()
+        {
+            Entities db = new Entities();
+            var data = db.Conductor.ToList();
+
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        // GET: Conductors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Conductor conductor = db.Conductor.Find(id);
+            if (conductor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(conductor);
+        }
+
+        // GET: Conductors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Conductors/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Codigo_Conductor,Nombre,Apellido,Direccion,Cedula,Telefono")] Conductor conductor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Conductor.Add(conductor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(conductor);
+        }
+
+        // GET: Conductors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Conductor conductor = db.Conductor.Find(id);
+            if (conductor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(conductor);
+        }
+
+        // POST: Conductors/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Codigo_Conductor,Nombre,Apellido,Direccion,Cedula,Telefono")] Conductor conductor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(conductor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(conductor);
+        }
+
+        // GET: Conductors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Conductor conductor = db.Conductor.Find(id);
+            if (conductor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(conductor);
+        }
+
+        // POST: Conductors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Conductor conductor = db.Conductor.Find(id);
+            db.Conductor.Remove(conductor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ajax-test/Scripts/main.js
@@ -0,0 +1,7 @@
+﻿var conductores = angular.module('conductores', []);
+conductores.controller('conductorescontroller', function ($scope, $http) {
+    $http.get('/Conductors/Index').then(function (response) {
+        $scope.lista = response.data;
+    })
+})
+
